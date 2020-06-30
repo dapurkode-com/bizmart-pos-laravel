1bff7394c101706d95796d905532d3db96214fe8
pandenengah <43629678+pandenengah@users.noreply.github.com>
2020-06-30 23:16:10 +0800

    manage crud user - commit2
    masih berkutat di insert

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -3,29 +3,29 @@
 @section('title', 'User | Bizmart')
 
 @section('content_header')
-<div class="row mb-2">
-	<div class="col-sm-6">
-        <blockquote style="margin: 0; background: unset;">
-            <h1 class="m-0 text-dark">USER</h1>
-        </blockquote>
-	</div>
-	<div class="col-sm-6">
-		<ol class="breadcrumb float-sm-right">
-			<li class="breadcrumb-item active">User</li>
-		</ol>
-	</div>
-</div>
+    <div class="row mb-2">
+        <div class="col-sm-6">
+            <blockquote style="margin: 0; background: unset;">
+                <h1 class="m-0 text-dark">USER</h1>
+            </blockquote>
+        </div>
+        <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+                <li class="breadcrumb-item active">User</li>
+            </ol>
+        </div>
+    </div>
 @stop
 
 @section('content')
-    <div class="row">
+    <div class="row subContent">
         <div class="col-sm-12">
             <div class="card">
                 <div class="card-header">
                     <h3 class="card-title">Daftar User</h3>
                     <div class="card-tools">
                         <div class="input-group input-group-sm"">
-                            <button class="btn btn-sm btn-primary"><i class="fas fa-plus" style="padding-right: 1rem;"></i>User Baru</button>
+                            <button class="btn btn-sm btn-primary btnAdd"><i class="fas fa-plus" style="padding-right: 1rem;"></i>User Baru</button>
                         </div>
                     </div>
                 </div>
@@ -42,6 +42,26 @@
                         </thead>
                         <tbody></tbody>
                     </table>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <!-- modal insert edit -->
+    <div class="modal fade" id="modalForm" tabindex="-1" role="dialog" aria-labelledby="modalFormLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="modalFormLabel">Inget Ganti Ne</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-primary">Inget Ganti Ne</button>
                 </div>
             </div>
         </div>
@@ -84,27 +104,9 @@
             },
         });
 
-        // $('#tbIndex').on('click', 'button[data-remote]', function (e) {
-        //     e.preventDefault();
-        //     $.ajaxSetup({
-        //         headers: {
-        //             'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-        //         }
-        //     });
-        //     var url = $(this).data('remote');
-
-        //     $.ajax({
-        //         url: url,
-        //         type: 'DELETE',
-        //         dataType: 'json',
-        //         data: {method: '_DELETE', submit: true, _token: '{{csrf_token()}}'},
-        //         beforeSend: function () {
-        //             return confirm('Apakah anda yakin ?');
-        //         }
-        //     }).always(function (data) {
-        //         $('#tbIndex').DataTable().draw(false);
-        //     });
-        // });
+        addListenToEvent('.subContent .btnAdd', 'click', (e) => {
+            $('#modalForm').modal('show');
+        });
     });
 
 
@@ -134,6 +136,18 @@
         }
         
     }
+
+    function addListenToEvent(elementSelector, eventName, handler) {
+        document.addEventListener(eventName, function(e) {
+            for (var target = e.target; target && target != this; target = target.parentNode) {
+                if (target.matches(elementSelector)) {
+                    handler.call(target, e);
+                    break;
+                }
+            }
+        }, false);
+    }
+
     // fixed function
 </script>
 @stop
