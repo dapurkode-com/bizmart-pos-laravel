2ce18c7f18dc4dc18742bec5e39207e537332da4
I Gusti Bagus Ngurah Satya Wibawa <i.g.b.n.satyawibawa@gmail.com>
2020-07-10 12:12:54 +0800

    Rearranged Routes

--- routes/web.php
+++ routes/web.php
@@ -23,28 +23,27 @@ Auth::routes(
     //    ]
 );
 
-Route::get('/home', 'HomeController@index')->name('home');
+
 
 
 Route::group(['middleware' => ['auth']], function () {
+
+    Route::get('/home', 'HomeController@index')->name('home');
     Route::get('suplier/datatables', 'SuplierController@datatables')->name('suplier.datatables');
-    Route::resource('suplier', 'SuplierController');
-
     Route::get('member/datatables', 'MemberController@datatables')->name('member.datatables');
-    Route::resource('member', 'MemberController');
-
     Route::get('item/datatables', 'ItemController@datatables')->name('item.datatables');
-    Route::resource('item', 'ItemController');
-
     Route::get('unit/datatables', 'UnitController@datatables')->name('unit.datatables');
-    Route::resource('unit', 'UnitController')->only('index', 'store', 'update', 'show', 'destroy');
-
     Route::get('category/datatables', 'CategoryController@datatables')->name('category.datatables');
-    Route::resource('category', 'CategoryController')->only('index', 'store', 'update', 'show', 'destroy');
-
     Route::get('user/datatables', 'UserController@datatables')->name('user.datatables');
-    Route::resource('user', 'UserController');
 
-
+    Route::resource('unit', 'UnitController')->only('index', 'store', 'update', 'show', 'destroy');
+    Route::resource('category', 'CategoryController')->only('index', 'store', 'update', 'show', 'destroy');
     Route::resource('buy', 'BuyController')->only('index', 'store', 'create', 'show', 'destroy');
+
+    Route::apiResources([
+        'suplier' => 'SuplierController',
+        'member' => 'MemberController',
+        'item'  => 'ItemController',
+        'user' => 'UserController'
+    ]);
 });
