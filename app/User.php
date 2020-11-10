8712b3097940c79e7a1b038921541fbaa7ae45ad
pandenengah <43629678+pandenengah@users.noreply.github.com>
2020-11-11 00:05:55 +0800

    user - day 1
    - fixing issue #38

--- app/User.php
+++ app/User.php
@@ -23,7 +23,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password', 'username'
+        'name', 'email', 'password', 'username', 'privilege_code', 'is_active'
     ];
 
     /**
