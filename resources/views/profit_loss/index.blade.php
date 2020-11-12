2d98fed7fecfbf792cf77ef368919029002797af
I Gusti Bagus Ngurah Satya Wibawa <i.g.b.n.satyawibawa@gmail.com>
2020-11-13 00:37:00 +0800

    Laporan Laba Rugi

--- resources/views/profit_loss/index.blade.php
+++ resources/views/profit_loss/index.blade.php
@@ -29,9 +29,12 @@
                             <div class="form-group mb-0">
                                <select name="month" id="month" class="form-control">
                                     @for ($i = 1; $i <= 12; $i++)
-                                        <option value="{{$i}}" {{Request::input('month', date('m')) == $i ? 'selected' : ''}}>{{DateTime::createFromFormat('!m', $i)->format('F')}}</option>
+                                        <option value="{{$i}}" {{$month == $i ? 'selected' : ''}}>{{DateTime::createFromFormat('!m', $i)->format('F')}}</option>
                                     @endfor
                                 </select>
+                            </div>
+                            <div class="form-group mb-0" style="width: 100px ">
+                                {!! Form::number('year',  $year, ['class'=>'form-control', 'style' => 'width:100%']) !!}
                             </div>
                             <button type="submit" class="btn btn-info filterButton"><i class="fas fa-search mr-2"></i>Cari</button>
                         </div>
@@ -61,30 +64,81 @@
                     <div class="col-sm-4 invoice-col"></div>
                     <div class="col-sm-4 invoice-col text-right">
                         <strong>Laporan Laba Rugi</strong><br>
-                        Bulan {{DateTime::createFromFormat('!m', $month)->format('F')}}
+                        Bulan {{DateTime::createFromFormat('!m', $month)->format('F')}} {{ $year }}
                     </div>
                </div>
                 <div class="row">
                     <div class="col-sm-12">
-                        <table class="table table-striped" id="tbIndex">
-                            <thead>
-                                <tr>
-                                    <th>#</th>
-                                    <th>Ref ID</th>
-                                    <th>Tanggal</th>
-                                    <th>Oleh</th>
-                                    <th>Keterangan</th>
-                                    <th>Arus</th>
-                                    <th class="text-right">Sejumlah</th>
-                                </tr>
-                            </thead>
-                            <tbody>
+                        <table id="profitTable" class="table table-hover" style="width: 100%">
+                        <tr>
+                            <th colspan="4">PENDAPATAN</th>
+                        </tr>
+                        <tr>
+                            <td colspan="2">Penjualan</td>
+                            <td>{{ number_format($sellSummary) }}</td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td colspan="2">Potongan Penjualan</td>
+                            <td style="border-bottom: 2px solid black;">0</td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <th colspan="2">JUMLAH PENDAPATAN BERSIH</th>
+                            <td></td>
+                            <td>{{ number_format($sellSummary) }}</td>
+                        </tr>
+                        <tr>
+                            <td colspan="4"></td>
+                        </tr>
+                        <tr>
+                            <th colspan="4">HARGA POKOK PENJUALAN (HPP)</th>
+                        </tr>
+                        <tr>
+                            <td colspan="1">Persediaan Barang Awal</td>
+                            <td>{{ number_format($stockValueOld) }}</td>
+                            <td></td><td></td>
+                        </tr>
+                        <tr>
+                            <td colspan="1">Pembelian</td>
+                            <td style="border-bottom: 2px solid black;">{{ number_format($buySummary) }}</td>
+                            <td></td><td></td>
+                        </tr>
+                        <tr>
+                            <td colspan="2">Barang Tersedia Dijual</td>
+                            <td>{{ number_format($buySummary + $stockValueOld) }}</td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td colspan="2">Persedian Barang Akhir</td>
+                            <td style="border-bottom: 2px solid black;">{{ number_format($stockValueNew) }}</td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <th colspan="3">HPP</th>
+                            <td style="border-bottom: 2px solid black;">{{ number_format($buySummary + $stockValueOld - $stockValueNew) }}</td>
+                        </tr>
+                        <tr>
+                            <th colspan="3">LABA KOTOR</th>
+                            <td>{{ number_format($sellSummary - ($buySummary + $stockValueOld - $stockValueNew)) }}</td>
+                        </tr>
+                        <tr>
+                            <td colspan="4"></td>
+                        </tr>
+                        <tr>
+                            <th colspan="4">BEBAN USAHA</th>
+                        </tr>
+                        <tr>
+                            <td colspan="1">Biaya Lainnya.</td>
+                            <td style="border-bottom: 2px solid black;">{{ number_format($otherExpenseSummary) }}</td>
+                            <td></td><td></td>
+                        </tr>
+                        <tr>
+                            <th colspan="3">LABA/RUGI BERSIH</th>
+                            <td>{{ number_format($sellSummary - ($buySummary + $stockValueOld - $stockValueNew) - $otherExpenseSummary) }}</td>
+                        </tr>
 
-                            </tbody>
-                            <tfoot>
-
-                            </tfoot>
-                        </table>
+                    </table>
                     </div>
                 </div>
                 <div class="row no-print text-right">
