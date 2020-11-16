09f6e3aea706abc8b0f4c6a288caf10e6712f4ad
I Gusti Bagus Ngurah Satya Wibawa <i.g.b.n.satyawibawa@gmail.com>
2020-11-16 18:40:22 +0800

    i18n

---
+++ resources/lang/vendor/adminlte/id/adminlte.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+    "full_name" => "Nama lengkap",
+    "email" => "Surel",
+    "password" => "Kata sandi",
+    "retype_password" => "Ketik ulang kata sandi",
+    "remember_me" => "Ingat saya",
+    "register" => "Daftar",
+    "register_a_new_membership" => "Daftarkan keanggotaan baru",
+    "i_forgot_my_password" => "Saya lupa kata sandi saya",
+    "i_already_have_a_membership" => "Saya sudah memiliki keanggotaan",
+    "sign_in" => "Masuk",
+    "log_out" => "Keluar",
+    "toggle_navigation" => "Alihkan navigasi",
+    "login_message" => "Silahkan Login Terlebih Dahulu",
+    "register_message" => "Daftarkan keanggotaan baru",
+    "password_reset_message" => "Atur Ulang Kata Sandi",
+    "reset_password" => "Atur Ulang Kata Sandi",
+    "send_password_reset_link" => "Kirim Tautan Reset Kata Sandi",
+    "verify_message" => "Akun Anda membutuhkan verifikasi",
+    "verify_email_sent" => "Tautan verifikasi baru telah dikirim ke alamat email Anda.",
+    "verify_check_your_email" => "Sebelum melanjutkan, periksa email Anda untuk tautan verifikasi.",
+    "verify_if_not_recieved" => "Jika Anda tidak menerima email",
+    "verify_request_another" => "klik di sini untuk meminta yang lain",
+    "confirm_password_message" => "Harap konfirmasi kata sandi Anda untuk melanjutkan."
+];
---
+++ resources/lang/vendor/adminlte/id/menu.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    "main_navigation" => "NAVIGASI UTAMA",
+    "blog" => "Blog",
+    "pages" => "Halaman",
+    "account_settings" => "PENGATURAN AKUN",
+    "profile" => "Profil",
+    "change_password" => "Ganti kata sandi",
+    "multilevel" => "Multi Level",
+    "level_one" => "Tingkat 1",
+    "level_two" => "Level 2",
+    "level_three" => "Tingkat 3",
+    "labels" => "LABEL",
+    "important" => "Penting",
+    "warning" => "Peringatan",
+    "information" => "Informasi"
+];
