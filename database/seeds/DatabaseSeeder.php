cb5acdc483be4350185637b77a204889ef98bc61
I Gusti Bagus Ngurah Satya Wibawa <i.g.b.n.satyawibawa@gmail.com>
2022-03-19 10:50:10 +0800

    Update DatabaseSeeder.php

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UserSeeder::class);
-        // factory(App\Item::class, 5)->create();
+        factory(App\Item::class, 5)->create();
         $this->call(LookUpsSeeder::class);
         $this->call(SystemParamsSeeder::class);
 
